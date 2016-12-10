c8dd2b2d27da63560ae477f9128205dc30152cb5
OPISTO\ville-petteri.sassi <vpsassi@gmail.com>
2016-12-10 15:49:58 +0200

    Ayy

---
+++ MeemiRotat/MeemiRotat/Assets/4Scripts/MummoScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MummoScript : MonoBehaviour {
+
+	public float scanSpeed;
+	public float alertSpeed;
+
+	float shotgunReloadTimer;
+	public float shotgunReloadTotTimer;
+
+	float aimTimer;
+	public float aimTotTimer;
+
+	public bool isOnSight;
+	public int playerCount;
+
+	public Transform[] wPs;
+	int nextWp;
+	float closeEnuf = 0.5f;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+
+	    transform.position = Vector2.MoveTowards(transform.position, wPs[nextWp].position, Time.deltaTime * scanSpeed);
+
+		if (Vector2.Distance(transform.position, wPs[nextWp].position)
+			< closeEnuf) {
+			nextWp = (Random.Range(0, wPs.Length)) % wPs.Length;
+
+		}
+
+		if (isOnSight == true) {
+			print("there ye are!");
+			aimTimer += Time.deltaTime;
+			if (aimTimer > aimTotTimer) {
+				print("boom");
+				aimTimer = 0;
+			}
+			else {
+				aimTimer = 0;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D c) {
+		if (c.tag == "Player") {
+			playerCount += 1;
+			isOnSight = true;
+		}
+	}
+	void OnTriggerExit2D (Collider2D c) {
+		if (c.tag == "Player") {
+			playerCount -= 1;
+			if(playerCount == 0) {
+		       isOnSight = false;
+			}
+	
+		}
+	} 
+}
---
+++ MeemiRotat/MeemiRotat/Assets/4Scripts/RatScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RatScript : MonoBehaviour {
+
+	public float moveSpeed;
+
+	public Animator rottaAnim;
+
+	void Start() {
+
+	}
+
+
+	void Update() {
+
+
+		if (Input.GetKey(KeyCode.W)) {
+			transform.Translate(Vector2.up * moveSpeed);
+			rottaAnim.SetBool("move", true);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			transform.Translate(Vector2.down * moveSpeed);
+			rottaAnim.SetBool("move", true);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			transform.Translate(Vector2.right * moveSpeed);
+			rottaAnim.SetBool("move", true);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Translate(Vector2.left * moveSpeed);
+			rottaAnim.SetBool("move", true);
+		}
+		else {
+			rottaAnim.SetBool("move", false);
+		}
+	}
+}
